71c35b3655e4aa29ed78cccb67aded928d9e443f
Clint Silvester <clint@gurutechnologies.net>
2017-10-18 18:53:39 +0000

    update these to use my server and make a few adjustments for this coin

--- q/height/index.php
+++ q/height/index.php
@@ -2,9 +2,10 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://52.21.253.162:32348/getinfo');
+curl_setopt($ch, CURLOPT_URL, 'http://explorer.mining.blue/domain/getinfo');
 $result = curl_exec($ch);
 $obj = json_decode($result, TRUE);
+//print_r($obj);
 curl_close($ch);
-print_r($obj['last_known_block_index']);
+print_r($obj['height']);
 ?>
--- q/supply/index.php
+++ q/supply/index.php
@@ -1,8 +1,9 @@
 <?php 
 
+
 $data_string = '{"jsonrpc":"2.0","id":"test","method":"getlastblockheader","params":" "}';
 
-$ch = curl_init('http://52.21.253.162:32348/json_rpc');
+$ch = curl_init('http://explorer.mining.blue/domain/json_rpc');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -13,13 +14,13 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 
 $result = curl_exec($ch);
 
-// print_r($result);
+//print_r($result);
 
 // Decode the response
 $responseData = json_decode($result, TRUE);
 
 // Print the date from the response
-// print_r($responseData);
+//print_r($responseData);
 
 $hash = $responseData['result']['block_header']['hash'];
 
@@ -29,7 +30,7 @@ curl_close($ch);
 $data_string2 = '{"jsonrpc":"2.0","id":"test","method":"f_block_json","params":{"hash":"'.$hash.'"}}';
 
 
-$ch2 = curl_init('http://52.21.253.162:32348/json_rpc');
+$ch2 = curl_init('http://explorer.mining.blue/domain/json_rpc');
 curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch2, CURLOPT_POSTFIELDS, $data_string2);
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
@@ -45,7 +46,7 @@ $blockData = json_decode($block, TRUE);
 
 //print_r($blockData);
 
-$supply = $blockData[result][block][alreadyGeneratedCoins];
+$supply = $blockData['result']['block']['alreadyGeneratedCoins'];
 
 $supply  = number_format($supply / 1000000000000, 12, ".", "");
 
